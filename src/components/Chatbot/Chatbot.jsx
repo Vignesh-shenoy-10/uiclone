3442b608add6ef590ffaa3115fedcefc890d0e5f
vignesh.shenoy <vignesh.shenoy@madvit.com>
2024-07-22 02:39:21 +0530

    Added send button

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -137,18 +137,18 @@ const ChatBot = () => {
                 className="search-bar-submit"
                 onClick={handleSearchSubmit}
               >
-                <svg
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12 2L19.09 9.09L16.59 11.59L13 8L12 9L9 6L4 11L2.5 9.5L12 2Z"
-                    fill="currentColor"
-                  />
-                </svg>
+<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M22 12L6 6v4l12 4-12 4v4L22 12z"
+    fill="currentColor"
+  />
+</svg>
               </button>
             </div>
           </div>
